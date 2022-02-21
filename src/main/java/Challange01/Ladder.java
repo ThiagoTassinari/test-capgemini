d971fd16886e582db198469d575067d577020f33
ThiagoTassinari <thiagotassinari12@gmail.com>
2022-02-20 22:36:22 -0300

    Finalizado questão 01

--- src/main/java/Challange01/Ladder.java
+++ src/main/java/Challange01/Ladder.java
@@ -12,23 +12,23 @@ public class Ladder {
 
     public static void main(String[] args) {
 
-        int asterisc = 0; 											//Nesse campo será armazenado o tamanho da escada
+        int asterisc = 0;
 
         Scanner keyboard = new Scanner(System.in);
 
         System.out.println("Descreva quantos andares a escada vai ter: ");
-        asterisc = keyboard.nextInt(); 						//Scanner do teclado sendo atribuído na variável asterisco
+        asterisc = keyboard.nextInt();
         System.out.println(" ");
 
         for(int i = 1; i <= asterisc; i++) {
             for(int j = 1; j <= asterisc - i; j++) {
-                System.out.print(' '); 							//Espaço em branco sendo atribuído por toda a linha
+                System.out.print(' ');
             }
             for(int j = 1; j <= i; j++) {
-                System.out.print('*');								//Asterisco sendo atribuído nos últimos espaços
+                System.out.print('*');
             }
-            System.out.println("");								//Salto de linha
+            System.out.println("");
         }
-        keyboard.close();                                              //Fechamento do Scanner (teclado)
+        keyboard.close();
     }
 }
